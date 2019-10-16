3338dce956056e6fabe5c9b7a8e1ff2af8975491
Paweł Broda <pwl.broda@gmail.com>
2019-10-16 20:02:33 +0200

    Automate preliminary-word-list check against duplicated words.

---
+++ scripts/validate_preliminary_list.py
@@ -0,0 +1,22 @@
+import re
+import sys
+
+with open("./preliminary-word-list") as f:
+    lines = f.readlines()
+
+    words = []
+    for line in lines:
+        word_match = re.match(r'(^\w+)\n', line)
+
+        if word_match is None:
+            continue
+
+        word = word_match.group(1)
+        if word in words:
+            print("error: first duplicated word on preliminary-word-list: '{}'".format(word))
+            sys.exit(1)
+
+        words.append(word)
+
+    print("success: preliminary-word-list has no duplicated words")
+    sys.exit(0)
