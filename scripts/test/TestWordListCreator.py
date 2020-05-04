e3be420293512e4ed0d70ebbc8be6de2418fadf5
p2w34 <36183210+p2w34@users.noreply.github.com>
2020-05-04 21:25:59 +0200

    Add create_sets_of_neighbours() method

--- scripts/test/TestWordListCreator.py
+++ scripts/test/TestWordListCreator.py
@@ -20,10 +20,10 @@ class TestWordListCreator(unittest.TestCase):
                 self.assertEqual(expected_list_of_words, WordListCreator().get_words(lines))
 
     scenarios_create_map_of_neighbours = [
-        (["aaaa"], {"aaaa":[]}),
-        (["aaaa", "bbbb"], {"aaaa":[], "bbbb":[]}),
-        (["aaaa", "aaab"], {"aaaa":["aaab"], "aaab":["aaaa"]}),
-        (["aaaa", "baaa", "abaa", "aaba", "aaab", "ffff"], {"aaaa":["baaa", "abaa", "aaba", "aaab"], "baaa":["aaaa"], "abaa":["aaaa"], "aaba":["aaaa"], "aaab":["aaaa"], "ffff":[]})
+        (["aaaa"], {"aaaa": set()}),
+        (["aaaa", "bbbb"], {"aaaa":set(), "bbbb":set()}),
+        (["aaaa", "aaab"], {"aaaa":set(["aaab"]), "aaab":set(["aaaa"])}),
+        (["aaaa", "baaa", "abaa", "aaba", "aaab", "ffff"], {"aaaa":set(["baaa", "abaa", "aaba", "aaab"]), "baaa":set(["aaaa"]), "abaa":set(["aaaa"]), "aaba":set(["aaaa"]), "aaab":set(["aaaa"]), "ffff":set()})
     ]
     def test_create_map_of_neighbours(self):
         for words, expected_map_of_neighbours in self.scenarios_create_map_of_neighbours:
@@ -31,16 +31,16 @@ class TestWordListCreator(unittest.TestCase):
                 self.assertEqual(expected_map_of_neighbours, WordListCreator().create_map_of_neighbours(words))
 
     scenarios_create_sets_of_neighbours = [
-        ({"aaaa":[]}, {frozenset(["aaaa"])}),
+        ({"aaaa":set()}, {frozenset(["aaaa"])}),
 
-        ({"aaaa":[], "bbbb":[]}, {frozenset(["aaaa"]), frozenset(["bbbb"])}),
-        #
-        # ({"aaaa":["aaab"], "aaab":["aaaa"]}, {frozenset(["aaaa", "aaab"])}),
-        #
-        # ({"aaaa":["baaa", "abaa", "aaba", "aaab"], "baaa":["aaaa"], "abaa":["aaaa"], "aaba":["aaaa"], "aaab":["aaaa"], "ffff":[]},
-        #  {frozenset(["aaaa", "baaa", "abaa", "aaba", "aaab"]), frozenset(["ffff"])}),
-        #
-        # ({"aaaa":["aaab"], "aaab":["aaaa"], "babb":["baab"], "baab":["babb", "aaab"]}, {frozenset(["aaaa", "aaab", "baab", "babb"])}),
+        ({"aaaa":set(), "bbbb":set()}, {frozenset(["aaaa"]), frozenset(["bbbb"])}),
+
+        ({"aaaa":set(["aaab"]), "aaab":set(["aaaa"])}, {frozenset(["aaaa", "aaab"])}),
+
+        ({"aaaa":set(["baaa", "abaa", "aaba", "aaab"]), "baaa":set(["aaaa"]), "abaa":set(["aaaa"]), "aaba":set(["aaaa"]), "aaab":set(["aaaa"]), "ffff":set()},
+         {frozenset(["aaaa", "baaa", "abaa", "aaba", "aaab"]), frozenset(["ffff"])}),
+
+        ({"aaaa":set(["aaab"]), "aaab":set(["aaaa"]), "babb":set(["baab"]), "baab":set(["babb", "aaab"])}, {frozenset(["aaaa", "aaab", "baab", "babb"])}),
     ]
     def test_create_sets_of_neighbours(self):
         for map_of_neighbours, expected_sets_of_neighbours in self.scenarios_create_sets_of_neighbours:
