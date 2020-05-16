42936f650d359e5dacbf73e7f9331f7e942aca47
Pawel Broda <pwl.broda@gmail.com>
2020-05-16 09:26:39 +0200

    refactor GraphUtils.py cd

--- scripts/src/GraphUtils.py
+++ scripts/src/GraphUtils.py
@@ -3,6 +3,11 @@ class GraphUtils:
     def __init__(self, neighbourhood_strategy):
         self.neighbourhood_strategy = neighbourhood_strategy
 
+    def extract_largest_set_without_neighbours_from_raw_set(self, words):
+        map_of_neighbours = self.create_map_of_neighbours(words)
+        word_set = self.extract_largest_set_without_neighbours_from_raw_map(map_of_neighbours)
+        return word_set
+
     def create_map_of_neighbours(self, words):
         map_of_neighbours = {}
         for w in words:
@@ -14,15 +19,12 @@ class GraphUtils:
 
         return map_of_neighbours
 
-    def extract_set_without_neighbours(self, map_of_neighbours):
-        sets_of_neighbours = self.create_sets_of_neighbours(map_of_neighbours)
-        print("Sets of neighbours: ", sets_of_neighbours)
-        print("Number of sets of neighbours: ", len(sets_of_neighbours))
-        word_set = self.extract_largest_set_without_neighbours(sets_of_neighbours)
-        print("Word set: ", word_set)
+    def extract_largest_set_without_neighbours_from_raw_map(self, map_of_neighbours):
+        sets_of_neighbours = self.split_into_isolated_sets_of_neighbours(map_of_neighbours)
+        word_set = self.extract_largest_set_without_neighbours_from_sets(sets_of_neighbours)
         return word_set
 
-    def create_sets_of_neighbours(self, map_of_neighbours):
+    def split_into_isolated_sets_of_neighbours(self, map_of_neighbours):
         sets_of_neighbours = set()
         for k, v in map_of_neighbours.items():
             new_set_of_neighbours = v
@@ -47,13 +49,18 @@ class GraphUtils:
 
         return sets_of_neighbours
 
-    # this is just a very basic stub and needs to be implemented properly
-    def extract_largest_set_without_neighbours(self, sets_of_neighbours):
+    def extract_largest_set_without_neighbours_from_sets(self, sets_of_neighbours):
         word_set = set()
         for set_of_neighbours in sets_of_neighbours:
-            if len(set_of_neighbours) == 1:
-                word_set.add(next(iter(set_of_neighbours)))
-            else:
-                1 == 1
-                # to implement: word_list.append(split_neighbours(set_of_neighbours))
+            word_subset = self.extract_largest_set_without_neighbours_from_set(set_of_neighbours)
+            word_set.update(word_subset)
         return word_set
+
+    # This is just a very basic stub and needs to be implemented properly.
+    # I suppose it is not possible to present the ideal implementation as for me it look like NP hard problem.
+    # However, one should be easily able to refactor it to increase size of the extracted set.
+    def extract_largest_set_without_neighbours_from_set(self, set_of_neighbours):
+        if len(set_of_neighbours) == 1:
+            return set_of_neighbours
+        else:
+            return set()
