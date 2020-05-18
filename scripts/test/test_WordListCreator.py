34e545a0d535bebdb3bc55098f53ab8595c68d69
Pawel Broda <pwl.broda@gmail.com>
2020-05-18 17:48:45 +0200

    implement WordListCreator.extract_words_with_unique_first_4_characters()

--- scripts/test/test_WordListCreator.py
+++ scripts/test/test_WordListCreator.py
@@ -17,7 +17,25 @@ class test_WordListCreator(unittest.TestCase):
     def test_get_words(self):
         for lines, expected_list_of_words in self.scenarios_get_words:
             with self.subTest():
-                self.assertEqual(expected_list_of_words, WordListCreator().get_words(lines))
+                self.assertEqual(
+                    expected_list_of_words,
+                    WordListCreator().get_words(lines)
+                )
+
+    scenarios_extract_words_with_unique_first_4_characters = [
+        (set(["aaaa"]), set(["aaaa"])),
+        (set(["aaaa", "bbbb"]), set(["aaaa", "bbbb"])),
+        (set(["aaaa", "bbbb", "cccc"]), set(["aaaa", "bbbb", "cccc"])),
+        (set(["aaaa", "aaaab"]), set(["aaaa"])),
+        (set(["aaaa", "bbbb", "cccc", "aaaab", "bbbba", "bbbbb", "cccca", "ccccb", "ccccc"]), set(["aaaa", "bbbb", "cccc"])),
+    ]
+    def test_extract_words_with_unique_first_4_characters(self):
+        for word_set, expected_word_set in self.scenarios_extract_words_with_unique_first_4_characters:
+            with self.subTest():
+                self.assertEqual(
+                    expected_word_set,
+                    WordListCreator().extract_words_with_unique_first_4_characters(word_set)
+                )
 
 if __name__ == '__main__':
     unittest.main()
