4a9f9494ae96c60fba000ad6e58c1fa7d99ce961
jasikor <jasikor@gmail.com>
2020-07-27 07:36:33 +0200

    last occurencess of english-770d1896-obsolete extinguished

--- scripts/compute_file_hash.py
+++ scripts/compute_file_hash.py
@@ -3,7 +3,7 @@ from scripts.src.file.FileHash import FileHash
 
 parser = argparse.ArgumentParser(
     description="Calculate SHA-3 hash of a file in hex form and outputs least significant 8 characters.",
-    epilog="Example: python3 scripts/compute_file_hash.py -f wlip-0003/english-770d1896-obsolete")
+    epilog="Example: python3 scripts/compute_file_hash.py -f wlip-0003/english-a1d03317-obsolete")
 parser.add_argument('-f', '--file', required=True)
 
 args = parser.parse_args()
